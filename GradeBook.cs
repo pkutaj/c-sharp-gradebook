de7ba82e2b094228c700972bd01dfd748c634387
pkutaj <pkutaj@gmail.com>
2019-10-17 14:23:22 +0200

    grades list explicitely typed

--- GradeBook.cs
+++ GradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,6 +8,9 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var numbers = new[] { 12.7, 10.3, 6.11 };
+            List<double> grades = new List<double>();
+            grades.Add(56.2);
+
             var result = 0.0;
 
             foreach (var number in numbers)
